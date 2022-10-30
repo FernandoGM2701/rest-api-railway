1a8578f77b8b6f18236d4570c7d85003cb582051
FernandoGM2701 <fernandogm2701@gmail.com>
2022-10-30 14:30:39 -0500

    [Add] get, post, put, delete routes

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const app = express();
 
 app.get('/teams', (req, res) => res.send("Obtener equipos"));
 
+app.post('/teams', (req, res) => res.send("Crear equipos"));
+
+app.put('/teams', (req, res) => res.send("Modificar equipos"));
+
+app.delete('/teams', (req, res) => res.send("Eliminar equipos"));
+
 app.listen(6900);
 console.log("Servidor corriendo en el puerto 6900");
